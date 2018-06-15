7d30fa1d590e810c58c49cf078537b773959ec06
root <root@localhost.localdomain>
2018-06-15 10:48:57 +0200

    Pandoc debug

--- action.php
+++ action.php
@@ -352,10 +352,11 @@ class action_plugin_odt2dw extends DokuWiki_Action_Plugin {
       $this->odtFileName = $this->userFileName.'.odt';
       $this->odtFile = $this->uploadDir.'/'. $this->odtFileName;
 
-      $output = array()
-      exec( 'pandoc -w odt -o ' . $this->odtFile . ' ' . $this->userFile, $output, $return_var );
+      $output = array();
+      exec( 'pandoc -s -w odt -o ' . $this->odtFile . ' ' . $this->userFile, $output, $return_var );
 
-      return this->_msg (array( 'er_pg_file', 'pandoc -w odt -o ' . $this->odtFile . ' ' . $this->userFile . ' Retorno: ' . $return_var );
+      # Debug log. Delete
+      # return $this->_msg (array( 'er_pg_file', 'pandoc -s -w odt -o ' . $this->odtFile . ' ' . $this->userFile . '. Salida: ' . $output[0] . '. Retorno: ' . $return_var ) );
     }
 
     // All upload file checking are OK
