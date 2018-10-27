f805972640079f05edfffaf10b2623c4e91ccf96
QKY <qky669@gmail.com>
2018-10-28 00:32:12 +0200

    Changed version number in action.php

--- action.php
+++ action.php
@@ -5,7 +5,7 @@
  * @license     GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author      Greg BELLAMY <garlik.crx@gmail.com> [Gag]
  * @author      José Torrecilla <qky669@gmail.com>
- * @version     0.10beta
+ * @version     0.11beta
  */
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
